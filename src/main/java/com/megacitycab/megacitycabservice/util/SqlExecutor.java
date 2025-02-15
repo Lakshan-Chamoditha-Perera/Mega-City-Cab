d190615e5c2636278689bcf487763ff6ea946014
Lakshan Chamoditha Perera <perera.alc2000@gmail.com>
2025-02-15 17:31:06 +0530

    SQL Executor utility added

---
+++ src/main/java/com/megacitycab/megacitycabservice/util/SqlExecutor.java
@@ -0,0 +1,16 @@
+package com.megacitycab.megacitycabservice.util;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@SuppressWarnings("unchecked")
+public abstract class SqlExecutor {
+    public static <T> T execute(Connection connection, String sql, Object... args) throws SQLException {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            for (int i = 0; i < args.length; i++)
+                preparedStatement.setObject(i + 1, args[i]);
+            return (sql.startsWith("SELECT") || sql.startsWith("select")) ? (T) preparedStatement.executeQuery() : (T) (Boolean) (preparedStatement.executeUpdate() > 0);
+        }
+    }
+}
