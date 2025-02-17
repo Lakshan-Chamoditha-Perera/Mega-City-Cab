a055648f9a3a18b56cfbcb38df2a2e1038ffa335
Lakshan Chamoditha Perera <perera.alc2000@gmail.com>
2025-02-17 09:38:16 +0530

    SqlExecutor added

--- src/main/java/com/megacitycab/megacitycabservice/repository/custom/impl/CustomerRepositoryImpl.java
+++ src/main/java/com/megacitycab/megacitycabservice/repository/custom/impl/CustomerRepositoryImpl.java
@@ -2,8 +2,12 @@ package com.megacitycab.megacitycabservice.repository.custom.impl;
 
 import com.megacitycab.megacitycabservice.entity.custom.Customer;
 import com.megacitycab.megacitycabservice.repository.custom.CustomerRepository;
+import com.megacitycab.megacitycabservice.util.SqlExecutor;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
@@ -12,60 +16,55 @@ public class CustomerRepositoryImpl implements CustomerRepository {
     private final Logger logger = Logger.getLogger(CustomerRepositoryImpl.class.getName());
 
     @Override
-    public Customer save(Customer entity, Connection connection) throws SQLException {
+    public Boolean save(Customer entity, Connection connection) throws SQLException {
         String sql = "INSERT INTO customer (firstName, lastName, email, nic, address, mobileNo, dateOfBirth) VALUES (?, ?, ?, ?, ?, ?, ?)";
-        try (PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
-            statement.setString(1, entity.getFirstName());
-            statement.setString(2, entity.getLastName());
-            statement.setString(3, entity.getEmail());
-            statement.setString(4, entity.getNic());
-            statement.setString(5, entity.getAddress());
-            statement.setString(6, entity.getMobileNo());
-            statement.setString(7, entity.getDateOfBirth().toString());
-            int affectedRows = statement.executeUpdate();
+//        try (PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+//            statement.setString(1, entity.getFirstName());
+//            statement.setString(2, entity.getLastName());
+//            statement.setString(3, entity.getEmail());
+//            statement.setString(4, entity.getNic());
+//            statement.setString(5, entity.getAddress());
+//            statement.setString(6, entity.getMobileNo());
+//            statement.setString(7, entity.getDateOfBirth().toString());
+//            int affectedRows = statement.executeUpdate();
+//
+//            if (affectedRows > 0) {
+//                try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+//                    if (generatedKeys.next()) {
+//                        entity.setCustomerId(generatedKeys.getInt(1));
+//                    }
+//                }
+//                return entity;
+//            }
+//            return null;
+//        }
 
-            if (affectedRows > 0) {
-                try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
-                    if (generatedKeys.next()) {
-                        entity.setCustomerId(generatedKeys.getInt(1));
-                    }
-                }
-                return entity;
-            }
-            return null;
-        }
+        return SqlExecutor.execute(
+                connection,
+                sql,
+                entity.getFirstName(),
+                entity.getLastName(),
+                entity.getEmail(),
+                entity.getNic(),
+                entity.getAddress(),
+                entity.getMobileNo(),
+                entity.getDateOfBirth().toString()
+        );
     }
 
     @Override
     public List<Customer> findAll(Connection connection) throws SQLException {
         String sql = "SELECT * FROM customer WHERE isDeleted = false";
-        List<Customer> customers = new ArrayList<>();
-        try (PreparedStatement statement = connection.prepareStatement(sql); ResultSet resultSet = statement.executeQuery()) {
-            while (resultSet.next()) {
-                customers.add(
-                        Customer.builder()
-                                .customerId(resultSet.getInt("customerId"))
-                                .firstName(resultSet.getString("firstName"))
-                                .lastName(resultSet.getString("lastName"))
-                                .email(resultSet.getString("email"))
-                                .nic(resultSet.getString("nic"))
-                                .address(resultSet.getString("address"))
-                                .mobileNo(resultSet.getString("mobileNo"))
-                                .dateOfBirth(Date.valueOf(resultSet.getString("dateOfBirth")))
-                                .build());
-            }
-        }
-        return customers;
-    }
 
-    @Override
-    public Customer findById(Integer id, Connection connection) throws SQLException {
-        String sql = "SELECT * FROM customer WHERE customerId = ? AND isDeleted = false";
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
-            statement.setInt(1, id);
-            try (ResultSet resultSet = statement.executeQuery()) {
-                if (resultSet.next()) {
-                    return Customer.builder()
+        ResultSet resultSet = SqlExecutor.execute(
+                connection,
+                sql
+        );
+        List<Customer> customers = new ArrayList<>();
+
+        while (resultSet.next()) {
+            customers.add(
+                    Customer.builder()
                             .customerId(resultSet.getInt("customerId"))
                             .firstName(resultSet.getString("firstName"))
                             .lastName(resultSet.getString("lastName"))
@@ -74,9 +73,32 @@ public class CustomerRepositoryImpl implements CustomerRepository {
                             .address(resultSet.getString("address"))
                             .mobileNo(resultSet.getString("mobileNo"))
                             .dateOfBirth(Date.valueOf(resultSet.getString("dateOfBirth")))
-                            .build();
-                }
-            }
+                            .build());
+        }
+        return customers;
+    }
+
+    @Override
+    public Customer findById(Integer id, Connection connection) throws SQLException {
+        String sql = "SELECT * FROM customer WHERE customerId = ? AND isDeleted = false";
+
+        ResultSet resultSet = SqlExecutor.execute(
+                connection,
+                sql,
+                id
+        );
+
+        if (resultSet.next()) {
+            return Customer.builder()
+                    .customerId(resultSet.getInt("customerId"))
+                    .firstName(resultSet.getString("firstName"))
+                    .lastName(resultSet.getString("lastName"))
+                    .email(resultSet.getString("email"))
+                    .nic(resultSet.getString("nic"))
+                    .address(resultSet.getString("address"))
+                    .mobileNo(resultSet.getString("mobileNo"))
+                    .dateOfBirth(Date.valueOf(resultSet.getString("dateOfBirth")))
+                    .build();
         }
         return null;
     }
@@ -84,29 +106,31 @@ public class CustomerRepositoryImpl implements CustomerRepository {
     @Override
     public boolean delete(Integer id, Connection connection) throws SQLException {
         String sql = "UPDATE customer SET isDeleted = true WHERE customerId = ?";
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
-            statement.setInt(1, id);
-            int affectedRows = statement.executeUpdate();
-            return affectedRows > 0;
-        }
+//        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+//            statement.setInt(1, id);
+//            int affectedRows = statement.executeUpdate();
+//            return affectedRows > 0;
+//        }
+
+        return SqlExecutor.execute(connection, sql, id);
+
     }
 
     @Override
     public Boolean updateById(Customer customer, Connection connection) throws SQLException {
-        System.out.println("UPDATE REPO");
         String sql = "UPDATE customer SET firstName = ?, lastName = ?, email = ?, nic = ?, address = ?, mobileNo = ?, dateOfBirth = ? WHERE customerId = ?";
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
-            statement.setObject(1, customer.getFirstName());
-            statement.setString(2, customer.getLastName());
-            statement.setString(3, customer.getEmail());
-            statement.setString(4, customer.getNic());
-            statement.setString(5, customer.getAddress());
-            statement.setString(6, customer.getMobileNo());
-            statement.setString(7, customer.getDateOfBirth().toString());
-            statement.setInt(8, customer.getCustomerId());
 
-            int affectedRows = statement.executeUpdate();
-            return affectedRows > 0;
-        }
+        return SqlExecutor.execute(
+                connection,
+                sql,
+                customer.getFirstName(),
+                customer.getLastName(),
+                customer.getEmail(),
+                customer.getNic(),
+                customer.getAddress(),
+                customer.getMobileNo(),
+                customer.getDateOfBirth().toString(),
+                customer.getCustomerId()
+        );
     }
 }
--- src/main/java/com/megacitycab/megacitycabservice/util/SqlExecutor.java
+++ src/main/java/com/megacitycab/megacitycabservice/util/SqlExecutor.java
@@ -7,10 +7,11 @@ import java.sql.SQLException;
 @SuppressWarnings("unchecked")
 public abstract class SqlExecutor {
     public static <T> T execute(Connection connection, String sql, Object... args) throws SQLException {
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-            for (int i = 0; i < args.length; i++)
-                preparedStatement.setObject(i + 1, args[i]);
-            return (sql.startsWith("SELECT") || sql.startsWith("select")) ? (T) preparedStatement.executeQuery() : (T) (Boolean) (preparedStatement.executeUpdate() > 0);
-        }
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        for (int i = 0; i < args.length; i++)
+            preparedStatement.setObject(i + 1, args[i]);
+            return (sql.startsWith("SELECT") || sql.startsWith("select")) ?
+                    (T) preparedStatement.executeQuery() :
+                    (T) (Boolean) (preparedStatement.executeUpdate() > 0);
     }
 }
