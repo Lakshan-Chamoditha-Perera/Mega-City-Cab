fb23b186bfe5e5921e60f68abb52350a5f6daa86
Lakshan Chamoditha Perera <perera.alc2000@gmail.com>
2025-02-24 22:18:25 +0530

    Driver Services added

--- src/main/java/com/megacitycab/megacitycabservice/repository/custom/DriverRepository.java
+++ src/main/java/com/megacitycab/megacitycabservice/repository/custom/DriverRepository.java
@@ -5,9 +5,16 @@ import com.megacitycab.megacitycabservice.repository.Repository;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 
 public interface DriverRepository extends Repository<Driver, Integer> {
 
     Boolean updateById(Driver driver, Connection connection) throws SQLException;
+
+    List <Driver> getAllDriversNotAssignedVehicle(Connection connection) throws SQLException;
+
+    Boolean updateDriverAvailability(Connection connection, boolean availability,Integer id) throws SQLException;
+
+    Integer getDriverAssignedVehicleCount(Integer id, Connection connection) throws SQLException;
 
 }
