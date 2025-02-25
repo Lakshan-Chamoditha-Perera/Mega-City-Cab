b28bdd1c5e618fa7a9646a4998141e73faa60922
Lakshan Chamoditha Perera <perera.alc2000@gmail.com>
2025-02-25 22:18:59 +0530

    getCount repo method added

--- src/main/java/com/megacitycab/megacitycabservice/repository/Repository.java
+++ src/main/java/com/megacitycab/megacitycabservice/repository/Repository.java
@@ -8,6 +8,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 public interface Repository<T extends Entity, K> {
+
     Boolean save(T entity, Connection connection) throws SQLException;
 
     List<T> findAll(Connection connection) throws SQLException;
@@ -15,4 +16,7 @@ public interface Repository<T extends Entity, K> {
     T findById(K id, Connection connection) throws SQLException;
 
     boolean delete(K id, Connection connection) throws SQLException;
+
+    Integer getCount(Connection connection) throws SQLException;
+
 }
