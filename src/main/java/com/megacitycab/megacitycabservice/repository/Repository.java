8c33102bb781730f0617ae8dfbe332a875d6457e
Lakshan Chamoditha Perera <perera.alc2000@gmail.com>
2025-02-28 22:37:34 +0530

    Main Repository updated

--- src/main/java/com/megacitycab/megacitycabservice/repository/Repository.java
+++ src/main/java/com/megacitycab/megacitycabservice/repository/Repository.java
@@ -1,22 +1,4 @@
 package com.megacitycab.megacitycabservice.repository;
 
-
-import com.megacitycab.megacitycabservice.entity.Entity;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.List;
-
-public interface Repository<T extends Entity, K> {
-
-    Boolean save(T entity, Connection connection) throws SQLException;
-
-    List<T> findAll(Connection connection) throws SQLException;
-
-    T findById(K id, Connection connection) throws SQLException;
-
-    boolean delete(K id, Connection connection) throws SQLException;
-
-    Integer getCount(Connection connection) throws SQLException;
-
+public interface Repository {
 }
